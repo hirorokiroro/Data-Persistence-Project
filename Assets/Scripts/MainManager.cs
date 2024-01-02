740cff1ef955b062f7123c0023744d4dfde84c46
hirorokiroro <140231450+hirorokiroro@users.noreply.github.com>
2024-01-02 16:55:27 +0900

    done
    I did all works

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+public static class GlobalVariables
+{
+    public static string playerName = "nanashi";
+    public static string bestPlayerName;
+    public static int bestScore = 0;
+
+}
 
 public class MainManager : MonoBehaviour
 {
@@ -12,9 +21,11 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+    [SerializeField] TextMeshProUGUI bestScoreText;
+    public Text playerName;
+
     private bool m_Started = false;
-    private int m_Points;
+    private int m_Points;//今回のゲームの合計得点
     
     private bool m_GameOver = false;
 
@@ -22,18 +33,25 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
+        GlobalVariables.bestPlayerName = PlayerPrefs.GetString("keyBestPlayer");
+        GlobalVariables.bestScore = PlayerPrefs.GetInt("keyBestScore");
+
+
+        playerName.text = "Player : "+ GlobalVariables.playerName;
+        bestScoreText.SetText("Best Score : " + GlobalVariables.bestPlayerName + " : " + GlobalVariables.bestScore);
+
+        const float step = 0.6f;//横並びのブロックの間隔と数を変える
+        int perLine = Mathf.FloorToInt(4.0f / step);//stepが大きいほど横並びのブロックの数が減る
         
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        int[] pointCountArray = new [] {1,1,2,2,5,5};//3種類のブロックの色ごとに点数の差をつける
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
             {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);//(x,y)座標
                 var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
+                brick.PointValue = pointCountArray[i];//ブロックの点数を配列の値とする
+                brick.onDestroyed.AddListener(AddPoint);//brickが無くなったときにAddPointというイベントを発行する
             }
         }
     }
@@ -62,7 +80,7 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    void AddPoint(int point)
+    void AddPoint(int point)//ブロックが無くなる度に呼び出される関数
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
@@ -72,5 +90,18 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+
+        if (GlobalVariables.bestScore < m_Points)
+        {
+            GlobalVariables.bestScore = m_Points;
+            GlobalVariables.bestPlayerName = GlobalVariables.playerName;
+        }
+        bestScoreText.SetText("Best Score : " + GlobalVariables.bestPlayerName + " : " + GlobalVariables.bestScore);
+
+        PlayerPrefs.SetInt("keyBestScore", GlobalVariables.bestScore);
+        PlayerPrefs.SetString("keyBestPlayer", GlobalVariables.bestPlayerName);
+
+        PlayerPrefs.Save();
+
     }
 }
